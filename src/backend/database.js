ae49cbd2d0775980918673570c984e0170493f75
Brendan Merritt <brm2000@live.unc.edu>
2022-04-21 17:33:36 -0400

    updated database with 3 tables, and defined more endpoints in API

--- src/backend/database.js
+++ src/backend/database.js
@@ -1,74 +1,75 @@
-import Database from "better-sqlite3";
+const db = require("better-sqlite3")("database.db");
 
-const db = new Database("database.db");
-
-const accountStmt = db.prepare(
-    `SELECT name FROM sqlite_master WHERE type='table' AND name='accounts';`
-);
-const logStmt = db.prepare(
-    `SELECT name FROM sqlite_master WHERE type='table' AND name='accesslog';`
-);
-const countyStmt = db.prepare(
-    `SELECT name FROM sqlite_master WHERE type='table' AND name='counties';`
-);
-
-let accountRow = accountStmt.get();
-let logRow = logStmt.get();
-let countyRow = countyStmt.get();
-
-if (accountRow === undefined) {
-    const accountInit = `
-        CREATE TABLE accounts (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            username TEXT,
-            emailaddr TEXT,
-            age TEXT,
-            profilepic TEXT
-        );
-    `;
-    db.exec(accountInit);
-    console.log("Account database has been created.");
-} else {
-    console.log("Account database already exists.");
+function dbInit() {
+    const accountStmt = db.prepare(
+        `SELECT name FROM sqlite_master WHERE type='table' AND name='accounts';`
+    );
+    const logStmt = db.prepare(
+        `SELECT name FROM sqlite_master WHERE type='table' AND name='accesslog';`
+    );
+    const countyStmt = db.prepare(
+        `SELECT name FROM sqlite_master WHERE type='table' AND name='counties';`
+    );
+    
+    let accountRow = accountStmt.get();
+    let logRow = logStmt.get();
+    let countyRow = countyStmt.get();
+    
+    if (accountRow === undefined) {
+        const accountInit = `
+            CREATE TABLE accounts (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                username TEXT,
+                emailaddr TEXT,
+                age TEXT,
+                profilepic TEXT,
+                admin TEXT
+            );
+        `;
+        db.exec(accountInit);
+        console.log("Account database has been created.");
+    } else {
+        console.log("Account database already exists.");
+    }
+    
+    if (logRow === undefined) {
+        const logInit = `
+            CREATE TABLE accesslog (
+                id INTEGER PRIMARY KEY,
+                remoteaddr TEXT,
+                remoteuser TEXT,
+                time NUMBER,
+                method TEXT,
+                url TEXT,
+                protocol TEXT,
+                httpversion NUMBER,
+                status NUMBER,
+                referer TEXT,
+                useragent TEXT
+            );    
+        `;
+        db.exec(logInit);
+        console.log("Interaction database has been created.");
+    } else {
+        console.log("Interaction database already exists.");
+    }
+    
+    if (countyRow === undefined) {
+        const logInit = `
+            CREATE TABLE counties (
+                id INTEGER PRIMARY KEY,
+                county TEXT,
+                dailycases NUMBER,
+                biweeklychange NUMBER,
+                fullyvax NUMBER
+            );    
+        `;
+        db.exec(logInit);
+        console.log("County database has been created.");
+    } else {
+        console.log("County database already exists.");
+    }
 }
 
-if (logRow === undefined) {
-    const logInit = `
-        CREATE TABLE accesslog (
-            id INTEGER PRIMARY KEY,
-            remoteaddr TEXT,
-            remoteuser TEXT,
-            time NUMBER,
-            method TEXT,
-            url TEXT,
-            protocol TEXT,
-            httpversion NUMBER,
-            status NUMBER,
-            referer TEXT,
-            useragent TEXT
-        );    
-    `;
-    db.exec(logInit);
-    console.log("Interaction database has been created.");
-} else {
-    console.log("Interaction database already exists.");
-}
-
-if (countyRow === undefined) {
-    const logInit = `
-        CREATE TABLE accesslog (
-            id INTEGER PRIMARY KEY,
-            county TEXT,
-            dailycases NUMBER,
-            biweeklychange NUMBER,
-            fullyvax NUMBER
-        );    
-    `;
-    db.exec(logInit);
-    console.log("County database has been created.");
-} else {
-    console.log("County database already exists.");
-}
-
-export {db};
+module.exports = {db, dbInit};
--- src/backend/server.js
+++ src/backend/server.js
@@ -1,16 +1,46 @@
-import express from "express";
-import fs from "fs";
-import morgan from "morgan";
-import {db} from "./database"
-
+const express = require("express");
 const app = express();
-const fileReader = fs;
+const {db, dbInit} = require("./database");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.listen(5000, () => {
-    console.log(`App is running on port 5000`);
+function appServer() {
+    dbInit();
+    const port = 5000;
+    app.listen(port, () => {
+        console.log(`App is running on port 5000`);
+    });
+}
+
+// start up the server
+appServer();
+
+// TO DO: middleware function(s) that inserts into database
+//app.use((req, res, next) => {});
+
+// debug endpoint for interaction logs - TO DO: restrict to admins only
+app.get('/app/logs', (req, res) => {
+    try {
+        const stmt = db.prepare(`SELECT * FROM accesslog`).all();
+        res.status(200).json(stmt);
+    } catch (e) {
+        console.error(e);
+    }
 });
 
-//need to finish
+// define check endpoint - TO DO: link to React homepage
+app.get('/app/', (req, res) => {
+    //respond with status 200
+    res.statusCode = 200;
+
+    //respond with status message "OK"
+    res.statusMessage = 'OK';
+    res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+    res.end(res.statusCode + ' ' + res.statusMessage);
+});
+
+// default endpoint
+app.use(function(req, res){
+    res.status(404).send("404 NOT FOUND");
+});
