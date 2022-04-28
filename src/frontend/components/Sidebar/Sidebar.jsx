c75b081fca2f8b4e821750703224e61b0d29bda8
KevinLan7 <kaiwenl@ad.unc.edu>
2022-04-27 21:29:07 -0400

    The whole frontend

--- src/frontend/components/Sidebar/Sidebar.jsx
+++ src/frontend/components/Sidebar/Sidebar.jsx
@@ -1,75 +1,86 @@
 import "./Sidebar.css";
-import {Home, Equalizer, Newspaper, List, ChatBubble, Notifications, Coronavirus, Healing} from '@mui/icons-material';
+import { Home, Equalizer, Newspaper, List, ChatBubble, Notifications, Coronavirus, Healing, Person } from '@mui/icons-material';
+import {
+    useNavigate,
+} from "react-router-dom";
 
 export default function Sidebar() {
-  return (
-    <div className="Sidebar">
-        <div className="SidebarWrapper">
-            <div className="SidebarMenu">
-                <h3 className="SidebarTitle">Dashboard</h3>
-                <ul className="SidebarList">
-                    <li className="SidebarListItem">
-                        <Home className="SidebarIcon"/>
-                        Overview 
-                    </li>
-                    <li className="SidebarListItem">
-                        <Equalizer className="SidebarIcon"/>
-                        Vaccine Statistics
-                    </li>
-                    <li className="SidebarListItem">
-                        <Coronavirus className="SidebarIcon"/>
-                        Outbreaks
-                    </li>
-                </ul>
-            </div>
+    let navigate = useNavigate();
 
-            <div className="SidebarMenu">
-                <h3 className="SidebarTitle">Public Resources</h3>
-                <ul className="SidebarList">
-                    <li className="SidebarListItem">
-                        <Newspaper className="SidebarIcon"/>
-                        News
-                    </li>
-                    <li className="SidebarListItem">
-                        <List className="SidebarIcon"/>
-                        FAQ
-                    </li>
-                    {/*<li className="SidebarListItem">
+    return (
+        <div className="Sidebar">
+            <div className="SidebarWrapper">
+                <div className="SidebarMenu">
+                    <h3 className="SidebarTitle">Dashboard</h3>
+                    <ul className="SidebarList">
+                        <li className="SidebarListItem">
+                            <Home className="SidebarIcon" />
+                            Overview
+                        </li>
+                        <li className="SidebarListItem">
+                            <Equalizer className="SidebarIcon" />
+                            Vaccine Statistics
+                        </li>
+                        <li className="SidebarListItem">
+                            <Coronavirus className="SidebarIcon" />
+                            Outbreaks
+                        </li>
+                        <li onClick={() => {
+                             navigate("/user")
+                        }} className="SidebarListItem">
+                            <Person className="SidebarIcon" />
+                            users
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="SidebarMenu">
+                    <h3 className="SidebarTitle">Public Resources</h3>
+                    <ul className="SidebarList">
+                        <li className="SidebarListItem">
+                            <Newspaper className="SidebarIcon" />
+                            News
+                        </li>
+                        <li className="SidebarListItem">
+                            <List className="SidebarIcon" />
+                            FAQ
+                        </li>
+                        {/*<li className="SidebarListItem">
                         <Map className="SidebarIcon"/>
                         Testing Sites
                     </li>*/}
-                    <li className="SidebarListItem">
-                        <Healing className="SidebarIcon"/>
-                        Symptoms
-                    </li>
-                </ul>
+                        <li className="SidebarListItem">
+                            <Healing className="SidebarIcon" />
+                            Symptoms
+                        </li>
+                    </ul>
 
-                
-            </div>
 
-            <div className="SidebarMenu">
-                <h3 className="SidebarTitle">Notifications</h3>
-                <ul className="SidebarList">
-                    <li className="SidebarListItem">
-                        <Home className="SidebarIcon"/>
-                        My Account
-                    </li>
-                    {/*<li className="SidebarListItem">
+                </div>
+
+                <div className="SidebarMenu">
+                    <h3 className="SidebarTitle">Notifications</h3>
+                    <ul className="SidebarList">
+                        <li className="SidebarListItem">
+                            <Home className="SidebarIcon" />
+                            My Account
+                        </li>
+                        {/*<li className="SidebarListItem">
                         <Map className="SidebarIcon"/>
                         My Testing Sites
                     </li>*/}
-                    <li className="SidebarListItem">
-                        <ChatBubble className="SidebarIcon"/>
-                        <span className="topbarIconBadge">2</span> 
-                        Messages
-                    </li>
-                    <li className="SidebarListItem">
-                        <Notifications className="SidebarIcon"/>
-                        Notifications
-                    </li>
-                </ul>
+                        <li className="SidebarListItem">
+                            <ChatBubble className="SidebarIcon" />
+                            <span className="topbarIconBadge">2</span>
+                            Messages
+                        </li>
+                        <li className="SidebarListItem">
+                            <Notifications className="SidebarIcon" />
+                            Notifications
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
